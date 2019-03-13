d7d3d0084590c23f0a169488b7cc1d96711a1b7b
Kaoru <KunoichiZ@users.noreply.github.com>
2019-03-13 00:35:31 -0500

    Update Releases.vue

--- pages/Releases.vue
+++ pages/Releases.vue
@@ -1,14 +1,130 @@
 <template>
-  <section class="container has-text-centered"><div>Releases</div>
-  
-  <!-- @todo Add Release Dates and Changelogs 
-       @body Add release dates and changelogs--></section>
+    <section class="container has-text-centered">
+        <div>
+        <br>
+        <h3 class="title is-3">Releases</h3>
+        </div>
+        <br>
+        <div id="threepoint1b3">
+            <h6 class="title is-6"><a href="https://github.com/rydoginator/ACNL-NTR-Cheats/releases/tag/V3.1B3">Version 3.1 Beta 3</a></h6>
+            <ul>
+                <li>Fixed inventory/appearance codes (again) for EUR and JAP</li>
+                <li>Fixed time travel codes for EUR and JAP</li>
+                <li>New 'midnight' command. Sets ingame time to 12:00 AM to the next day.</li>
+                <li>Real time world edit changes: copy and paste function. Press L + D pad up to store the item and item tag you're standing on, press L + D pad down to write the stored item to where you're standing.</li>
+            </ul>
+            <br>
+            <b>Download link:</b> <a>https://github.com/rydoginator/ACNL-NTR-Cheats/releases/download/V3.1B3/ACNL_MULTI.plg</a>
+        </div>
+        <br>
+        <div id="threepoint1b2">
+            <h6 class="title is-6"><a href="https://github.com/rydoginator/ACNL-NTR-Cheats/releases/tag/v3.1B2">Version 3.1 Beta 2</a></h6>
+            <ul>
+                <li>Merged all codes to universally work in one function per cheat thanks to <a href="https://github.com/Nanquitas">@Nanquitas</a></li>
+                <li>Fixed text cheats thanks to <a href="https://github.com/Nanquitas">@Nanquitas</a></li>
+                <li>Text to cheat thanks to <a href="https://github.com/Nanquitas">@Nanquitas</a> only 2 cheats for now, but I'll add more later
+                <li>Commands (remove quotes when sending into chat):
+                    <ul>
+                        <li>'quench' - water flowers</li>
+                        <li>'savetp' - save teleport location</li>
+                        <li>'restoretp' - teleport to saved location</li>
+                        <li>'bis' - execute last command</li>
+                    </ul>
+                </li>
+            </ul>
+            <br>
+            <b>Download link:</b> <a>https://github.com/rydoginator/ACNL-NTR-Cheats/releases/download/v3.1B2/ACNL_MULTI.plg</a>
+        </div>
+        <br>
+        <div id="threepoint1b1">
+            <h6 class="title is-6"><a href="https://github.com/rydoginator/ACNL-NTR-Cheats/releases/tag/v3.1B1">Version 3.1 Beta 1</a></h6>
+            <ul>
+                <li>Dynamic tan modifier thanks to <a href="https://github.com/Nanquitas">@Nanquitas</a></li>
+                <li>Walk through walls thanks to dsrules</li>
+                <li>SeederV2 from 3.0 Beta replaced SeederV1 for Japan only atm</li>
+            </ul>
+            <br>
+            More soon!
+            <br>
+            <br><b>Download link:</b> <a>https://github.com/rydoginator/ACNL-NTR-Cheats/releases/download/v3.1B1/ACNL_MULTI.plg</a>
+        </div>
+        <br>
+        <div id="threepoint0point1">
+            <h6 class="title is-6"><a href="https://github.com/rydoginator/ACNL-NTR-Cheats/releases/tag/v3.0.1">Version 3.0.1</a></h6>
+            <ul>
+                <li>Full Japan support</li>
+                <li>Added notes to almost every cheat entry to avoid confusion</li>
+                <li>Tan modifier</li>
+                <li>Fixed time travel/time machine for EUR</li>
+            </ul>
+            <br>
+            <b>Download link:</b> <a>https://github.com/rydoginator/ACNL-NTR-Cheats/releases/download/v3.0.1/ACNL_MULTI.plg</a>
+        </div>
+        <br>
+        <div id="threepoint0">
+            <h6 class="title is-6"><a href="https://github.com/rydoginator/ACNL-NTR-Cheats/releases/tag/v3.0">Version 3.0</a></h6>
+            <ul>
+                <li>USA support for Amiibo update and ported to EUR thanks to <a href="https://github.com/Slattz">@Slattz</a></li>
+                <li>Real Time World Edit added thanks to <a href="https://github.com/conanac">@conanac</a> for the help!</li>
+                <li>Text to item and duplication now works for player 2-4 in one cheat</li>
+                <li>Search and replace uses XXXXYYYY for USA now thanks to <a href="https://github.com/Nanquitas">@Nanquitas</a></li>
+                <li>Time machine and time travel added thanks to edwardmanuel</li>
+                <li>Moved all R+A codes into one spoiler to prevent enabling more than one at a time</li>
+                <li>Duplication/text2item may or may not be broken online, please give me feedback.</li>
+            </ul>
+            <br>
+            <b>Download link:</b> <a>https://github.com/rydoginator/ACNL-NTR-Cheats/releases/download/v3.0/ACNL_MULTI.plg</a>
+        </div>
+        <br>
+        <div id="threepoint0b1">
+            <h6 class="title is-6"><a href="https://github.com/rydoginator/ACNL-NTR-Cheats/releases/tag/v3.0B1">Version 3.0 Build 1</a></h6>
+            <ul>
+                <li>Europe and USA only.</li>
+                <li>Time Travel: press B + d pad right to fast forward an hour, press B + d pad right to rewind an hour. Press B + D pad down to revert the ingame time to the system time.</li>
+                <li>Press R + D pad right/left to watch time rewind/fast forward with no restrictions on keypresses.</li>
+                <li>Time Machine: Type YYMMDDHHmm in chat and send it where YY is year, MM is month, DD is day HH is hour, and mm is minute. Press Y + d pad right to fast forward that amount and press Y + d pad left to rewind that amount.</li>
+            </ul>
+            <br>
+            Props to edwardmanuel for finding this <a href="https://github.com/rydoginator/ACNL-NTR-Cheats/issues/13">#13</a>
+            <br><br>
+            <b>Download link:</b> <a>https://github.com/rydoginator/ACNL-NTR-Cheats/releases/download/v3.0B1/ACNL_MULTI.plg</a>
+        </div>
+    </section>
 </template>
 
 <script>
 
 </script>
 
-<style>
+<style scoped>
+#threepoint0b1, #threepoint0, #threepoint0point1, #threepoint1b1, #threepoint1b2, #threepoint1b3 {
+  text-align: left;
+  margin: auto;
+}
 
+#threepoint0b1, #threepoint1b1 {
+  padding: 15px;
+  background: url('/blue_bg.png');
+  width: 1000px;
+  border-radius: 25px;
+}
+
+#threepoint0, #threepoint1b2 {
+  padding: 15px;
+  background: url('/pink_bg.png');
+  width: 1000px;
+  border-radius: 25px;
+}
+
+#threepoint0point1, #threepoint1b3 {
+  padding: 15px;
+  background: url('/green_bg.png');
+  width: 1000px;
+  border-radius: 25px;
+}
+
+#threepoint0b1 ul, #threepoint0 ul, #threepoint0point1 ul, #threepoint1b1 ul, #threepoint1b2 ul, #threepoint1b3 ul {
+  list-style-type: disc;
+  list-style-position: inside;
+}
 </style>
