244e7785549b54701adde9d2adec011da6bab13d
Kaoru <KunoichiZ@users.noreply.github.com>
2019-03-13 00:50:02 -0500

    Update Releases.vue

--- pages/Releases.vue
+++ pages/Releases.vue
@@ -5,6 +5,86 @@
         <h3 class="title is-3">Releases</h3>
         </div>
         <br>
+        <div id="threepoint1point3"> <!-- blue -->
+            <h6 class="title is-6"><a href="https://github.com/rydoginator/ACNL-NTR-Cheats/releases/tag/v3.1.3">Version 3.1.3</a></h6>
+            What's New
+            <ul>
+                <li>Fixed 'clearinv' command</li>
+                <li>Organized the layout according to surveys</li>
+                <li>Changed walk through walls and walk over things keypresses to L + D pad</li>
+            </ul>
+            <br>
+            <b>Download link:</b> <a>https://github.com/rydoginator/ACNL-NTR-Cheats/releases/download/v3.1.3/ACNL_MULTI.plg</a>
+        </div>
+        <br>
+        <div id="threepoint1point2">
+            <h6 class="title is-6"><a href="https://github.com/rydoginator/ACNL-NTR-Cheats/releases/tag/v3.1.2">Version 3.1.2</a></h6>
+            What's New
+            <ul>
+                <li>Fixed inventory while online
+                <li>Added dynamic slot 1 modifier
+                <li>Added walk over objects</li>
+            </ul>
+            <br>
+            How to use Dynamic Slot Mod
+            <ol>
+                <li>Make sure you're on Club Tortimer</li>
+                <li>Get your item ID from this website <a>http://sirbeethoven.github.io/LeafHacks/</a></li>
+                <li>Convert it to decimal by entering the item ID into a hex to decimal converter. Use this site <a>http://www.binaryhexconverter.com/hex-to-decimal-converter</a></li>
+                <li>Type in the ID into an ABD numpad and then press Y + D Pad Right to write the item to slot 01.</li>
+            </ol>
+            <br>
+            <b>Download link:</b> <a>https://github.com/rydoginator/ACNL-NTR-Cheats/releases/download/v3.1.2/ACNL_MULTI.plg</a>
+        </div>
+        <br>
+        <div id="threepoint1">
+            <h6 class="title is-6"><a href="https://github.com/rydoginator/ACNL-NTR-Cheats/releases/tag/v3.1">Version 3.1</a></h6>
+            The long awaited update!
+            <ul>
+                <li>Online item spawning makes a return!</li>
+                <li>Edible items makes a return!</li>
+                <li>Seeder now works by pressing R while dropping an item</li>
+                <li>Tons of write protection to prevent writing to wrong offsets</li>
+                <li>Walk through walls thanks to dsrules</li>
+                <li>Dynamic tan modifier thanks to <a href="https://github.com/Nanquitas">@Nanquitas</a></li>
+                <li>Text to cheat</li>
+                <li>Real time world edit changes: copy and paste function. Press L + D pad up to store the item and item tag you're standing on, press L + D pad down to write the stored item to where you're standing.</li>
+                <li>Full support for the 1.5 patch</li>
+            </ul>
+            <br>
+            As a workaround to allow spawning items on the island, I've added a option to read item IDS from the keyboard, and been improved with the help of <a href="https://github.com/Nanquitas">@Nanquitas</a>.
+            <br><br>
+            Text to cheat Commands:
+            <br>
+            <ul>
+                <li>'midnight' - sets ingame time to midnight</li>
+                <li>'noon' - sets ingame time to noon</li>
+                <li>'morning' - sets ingame time to morning</li>
+                <li>'stalk1' - teleports you to the town host or first person to join your town</li>
+                <li>'gorgeset' fills your inventory with the gorgeous set</li>
+                <li>'clearinv' - clears your inventory</li>
+                <li>'quench' - water flowers</li>
+                <li>'savetp' - save teleport location</li>
+                <li>'restoretp' - teleport to saved location</li>
+                <li>'bis' - execute last command</li>
+            </ul>
+            <br>
+            <b>Download link:</b> <a>https://github.com/rydoginator/ACNL-NTR-Cheats/releases/download/v3.1/ACNL_MULTI.plg</a>
+        </div>
+        <br>
+        <div id="threepoint1b4">
+            <h6 class="title is-6"><a href="https://github.com/rydoginator/ACNL-NTR-Cheats/releases/tag/v3.1B4">Version 3.1 Beta 4</a></h6>
+            <ul>
+                <li>Online inventory codes for all regions</li>
+                <li>GorgeSet command (gives you gorgeous set)</a>
+                <li>Clearinv command</a>
+                <li>Full support for 1.5 EUR + JAP patch</a>
+                <li>Add protection to determine whether you're in your town or not to prevent online offsets from not executing</li>
+            </ul>
+            <br>
+            <b>Download link:</b> <a>https://github.com/rydoginator/ACNL-NTR-Cheats/releases/download/v3.1B4/ACNL_MULTI.plg</a>
+        </div>
+        <br>
         <div id="threepoint1b3">
             <h6 class="title is-6"><a href="https://github.com/rydoginator/ACNL-NTR-Cheats/releases/tag/V3.1B3">Version 3.1 Beta 3</a></h6>
             <ul>
@@ -97,34 +177,38 @@
 </script>
 
 <style scoped>
-#threepoint0b1, #threepoint0, #threepoint0point1, #threepoint1b1, #threepoint1b2, #threepoint1b3 {
+#threepoint0b1, #threepoint0, #threepoint0point1, #threepoint1b1, #threepoint1b2, #threepoint1b3, #threepoint1b4, #threepoint1, #threepoint1point2, #threepoint1point3 {
   text-align: left;
   margin: auto;
 }
 
-#threepoint0b1, #threepoint1b1 {
+#threepoint0b1, #threepoint1b1, #threepoint1b4, #threepoint1point3 {
   padding: 15px;
   background: url('/blue_bg.png');
   width: 1000px;
   border-radius: 25px;
 }
 
-#threepoint0, #threepoint1b2 {
+#threepoint0, #threepoint1b2, #threepoint1 {
   padding: 15px;
   background: url('/pink_bg.png');
   width: 1000px;
   border-radius: 25px;
 }
 
-#threepoint0point1, #threepoint1b3 {
+#threepoint0point1, #threepoint1b3, #threepoint1point2 {
   padding: 15px;
   background: url('/green_bg.png');
   width: 1000px;
   border-radius: 25px;
 }
 
-#threepoint0b1 ul, #threepoint0 ul, #threepoint0point1 ul, #threepoint1b1 ul, #threepoint1b2 ul, #threepoint1b3 ul {
+#threepoint0b1 ul, #threepoint0 ul, #threepoint0point1 ul, #threepoint1b1 ul, #threepoint1b2 ul, #threepoint1b3 ul, #threepoint1b4 ul, #threepoint1 ul, #threepoint1point2 ul, #threepoint1point3 ul {
   list-style-type: disc;
+  list-style-position: inside;
+}
+
+#threepoint1point2 ol {
   list-style-position: inside;
 }
 </style>
