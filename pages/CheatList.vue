447747948260daeacca83bc5e347f6a614878410
Kaoru <KunoichiZ@users.noreply.github.com>
2019-03-25 17:00:51 -0500

    Create CheatList.vue

---
+++ pages/CheatList.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="container has-text-centered"><div>Cheat List</div>
+  
+      // TODO: Add Cheat List
+      
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
