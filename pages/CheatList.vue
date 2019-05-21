8c7ad8ac4983b39525b247ede7f235563f512735
Kaoru <19984244+KunoichiZ@users.noreply.github.com>
2019-05-21 03:55:10 -0500

    Update CheatList.vue

--- pages/CheatList.vue
+++ pages/CheatList.vue
@@ -1,7 +1,133 @@
 <template>
-  <section class="container has-text-centered"><div>Cheat List</div>
-  
-      // TODO: Add Cheat List
+  <section class="container has-text-centered">
+    <br>
+    <div><h1 class="is-size-3">Cheat List</h1>
+      <br>
+      <div id="save-file-cheats">
+        <h3 class="is-size-5">Save File Cheats:</h3>
+        <ul>
+          <li>Set name to...</li>
+          <li>Save Dumper</li>
+          <li>Save Restore</li>
+          <li>Change Town Fruit to...</li>
+          <li>Change Town Grass to...</li>
+          <li>Real Time Building Modifier</li>
+        </ul>
+      </div>
+      <br>
+      <div id="movement-cheats">
+        <h3 class="is-size-5">Movement Cheats:</h3>
+        <ul>
+          <li>Coordinates Mod</li>
+          <li>Touch Coordinates</li>
+          <li>Teleport</li>
+          <li>Walk Over Things (DPAD UP+L)</li>
+          <li>Speed Hack</li>
+          <li>Moon Jump</li>
+          <li>Teleport to PWP</li>
+        </ul>
+      </div>
+      <br>
+      <div id="time-travel-cheats">
+        <h3 class="is-size-5">Time Travel:</h3>
+        <ul>
+          <li>Time Machine</li>
+          <li>Time Travel</li>
+        </ul>
+      </div>
+      <br>
+      <div id="unlock-cheats">
+        <h3 class="is-size-5">Unlock:</h3>
+        <ul>
+          <li>100% Mayor Permit</li>
+          <li>Unlock All PWPs</li>
+          <li>Fill Out Encyclopedia</li>
+          <li>Fill Out Emoticons</li>
+          <li>Fill Out K.K. Songs</li>
+          <li>Fill Out Catalog</li>
+        </ul>
+      </div>
+      <br>
+      <div id="environment-cheats">
+        <h3 class="is-size-5">Environment:</h3>
+        <ul>
+          <li>R+A Cheats</li>
+          <ul>
+            <li>Remove All Items</li>
+            <li>Remove All Weeds</li>
+            <li>Water All Flowers</li>
+            <li>Fill Grass</li>
+            <li>Destroy Grass</li>
+          </ul>
+          <li>Real Time World Edit</li>
+          <li>Search and Replace</li>
+          <li>Flowers are Unbreakable (DPAD RIGHT+R)</li>
+        </ul>
+      </div>
+      <br>
+      <div id="inventory-cheats">
+        <h3 class="is-size-5">Inventory Cheats:</h3>
+        <ul>
+          <li>Money Cheats</li>
+          <ul>
+            <li>Max/Infinite Bank</li>
+            <li>Max/Infinite Wallet</li>
+            <li>Max/Infinite Coupons</li>
+            <li>Max/Infinite Island Medals</li>
+            <li>Bells In Bank Don't Decrease</li>
+            <li>Bells in Wallet Don't Decrease</li>
+            <li>Coupons Don't Decrease</li>
+            <li>Medals Don't Decrease</li>
+            <li>Wallet Editor</li>
+            <li>Bank Editor</li>
+          </ul>
+          <li>Text to Item (DPAD RIGHT+X)</li>
+          <li>Duplicate (R)</li>
+          <li>Pick Up Buried Items</li>
+          <li>Inventory Box Extender</li>
+          <li>Fossil Inspector</li>
+        </ul>
+      </div>
+      <br>
+      <div id="shop-cheats">
+        <h3 class="is-size-5">Shop Cheats:</h3>
+        <ul>
+          <li>Nookling Upgrades</li>
+          <li>Main Street Shops</li>
+          <li>Shops Always Open</li>
+          <li>Catalog to Pockets (L)</li>
+        </ul>
+      </div>
+      <br>
+      <div id="misc-cheats">
+        <h3 class="is-size-5">Misc Cheats:</h3>
+        <ul>
+          <li>Ghost Mode (DPAD UP+Y)</li>
+          <li>Camera Mod</li>
+          <li>Country Spoofer (B+L)</li>
+          <li>Custom Symbols Keyboard</li>
+          <li>Keyboard Extender</li>
+          <li>Fast Game Speed</li>
+          <li>Item Form Changer</li>
+          <li>Item Effect Changer</li>
+          <li>Special NPC Changer</li>
+          <li>Access Catalog & Storage Anywhere</li>
+          <li>Emote ID Changer</li>
+          <li>Amiibo Spoofer</li>
+          <li>Pick Every Tour</li>
+          <li>Unburying (R)</li>
+          <li>Weed Pulling (R)</li>
+          <li>Weather Mod</li>
+          <li>Edit Every Pattern (DPAD RIGHT+R)</li>
+          <li>Faint (A+R)</li>
+          <li>Cherry Blossom Mod</li>
+          <li>Confetti Mod</li>
+        </ul>
+      </div>
+    </div>
+      <br>
+      // TODO: Add Button Commands to Necessary Commands
+      // TODO: Add Table of Contents
       
   </section>
 </template>
@@ -10,6 +136,14 @@
 
 </script>
 
-<style>
+<style scoped>
+#save-file-cheats, #movement-cheats, #time-travel-cheats, #unlock-cheats, #environment-cheats, #shop-cheats, #misc-cheats, #inventory-cheats {
+  text-align: left;
+  margin-left: 60px;
+}
 
+ul {
+  list-style-type: circle;
+  margin-left: 20px;
+}
 </style>
