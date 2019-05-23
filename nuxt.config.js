d7e2eb7b74a513e7150858caf06b49a005d97517
Kaoru <19984244+KunoichiZ@users.noreply.github.com>
2019-05-23 02:13:56 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,8 +14,11 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
+    script: [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js' }
+    ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
     ]
   },
 
