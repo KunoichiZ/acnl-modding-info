9a9632eec8ad70debb341bd80302148b51e8ac05
Kaoru <19984244+KunoichiZ@users.noreply.github.com>
2019-05-23 02:14:31 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <section class="container has-text-centered"><div>Home</div></section>
+  <div class="container has-text-centered">
+    <br><h1 class="is-size-3">Animal Crossing: New Leaf Modding Information</h1>
+    <br>This is the website repo for the ACNL Modding Information website for the ACNL Modding Discord Server.
+
+    <br><br>The links above will take you to their respective pages, which will show you the <nuxt-link to="/cheatlist">cheat list</nuxt-link>, the <nuxt-link to="/itemids">Item IDs</nuxt-link> for the Text to Item cheat, 3DS and ACNL <nuxt-link to="/jargon">jargon</nuxt-link>, <nuxt-link to="/guides">guides</nuxt-link>, <nuxt-link to="/faq">frequently asked questions</nuxt-link>, and the <nuxt-link to="/releases">releases</nuxt-link> of the plugin.
+
+    <br><br><u>Questions about the site?</u>
+    <br>DM <b-tooltip label="AinoMinako#7220" dashed>@KunoichiZ</b-tooltip>AinoMinako#7220 on Discord.
+
+    <br><br><u>Issues with the site or the content on it?</u>
+    <br>Open an <a href="https://github.com/KunoichiZ/acnl-modding-info/issues">issue</a> or make a <a href="https://github.com/KunoichiZ/acnl-modding-info/pulls">pull request</a>. When making a pull request, please test all code changes before submitting the pull request. Any changes that fail to build will be automatically rejected until they are fixed. For more information on issues and pull requests, please visit the <a href="https://github.com/KunoichiZ/acnl-modding-info/blob/master/.github/ISSUE_TEMPLATE.md">issue template</a> and the <a href="https://github.com/KunoichiZ/acnl-modding-info/blob/master/.github/PULL_REQUEST_TEMPLATE.md">pull request template</a>.
+  </div>
 </template>
 
 <script>
