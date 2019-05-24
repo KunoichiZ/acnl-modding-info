b11302c60856a4a9efb47ff68623db109d8d256b
Kaoru <19984244+KunoichiZ@users.noreply.github.com>
2019-05-24 15:36:23 -0500

    Create Installation.vue

---
+++ pages/Installation.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container has-text-centered">
+    <div>Installation</div>
+  
+      // TODO: Add Installation page
+      
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
