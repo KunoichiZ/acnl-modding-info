2ccf5bdaffad072bfa030aef6492954c7f02f9d5
Kaoru <19984244+KunoichiZ@users.noreply.github.com>
2019-05-24 17:37:01 -0500

    Update vue-discord-messages.js

--- plugins/vue-discord-messages.js
+++ plugins/vue-discord-messages.js
@@ -1,4 +1,17 @@
 import Vue from 'vue';
 import VueDiscordMessage from 'vue-discord-message';
  
-Vue.use(VueDiscordMessage);
+Vue.use(VueDiscordMessage, {
+    profiles: {
+        nanashi: {
+            author: 'nanashi89',
+            avatar: 'https://i.imgur.com/6XRINSA.png',
+            roleColor: '#e67e22'
+        },
+        kunoichi: {
+            author: 'KunoichiZ',
+            avatar: 'https://i.imgur.com/7IWUDBD.png',
+            roleColor: '#e4fa24'
+        },
+    },
+});
