6ae8619e80171b6532c3d3034f5caa8a2cdcd8b7
Kaoru <19984244+KunoichiZ@users.noreply.github.com>
2019-05-25 00:59:57 -0500

    Create ClothesTable.vue

---
+++ components/ClothesTable.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <input type="text" id="clothesInput" v-on:keyup="clothesFunction()" placeholder="Search for item names..">
+        <br><br>
+        <table id="clothesTable">
+            <tr>
+  	            <th colspan="2" id="clothes">Clothes (back to <a href="#top">categories</a>)</th>
+            </tr>
+            <tr>
+  	            <th>Hex</th>
+                <th>Item Name</th>
+            </tr>
+            <!-- <tr>
+                <td>30d2</td>
+                <td>dynamic painting</td>
+            </tr> -->
+        </table>
+    </div>
+</template>
+
+<script>
+function clothesFunction() {
+  // Declare variables 
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("clothesInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("clothesTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
+
+export default {
+methods: {
+    clothesFunction
+  }
+}
+</script>
+
+<style scoped>
+table, td {
+  border: 1px solid black;
+  text-align: center;
+}
+
+th {
+  border: 2px solid black;
+  text-align: center;
+}
+
+table {
+  margin: auto;
+}
+
+th, td {
+    padding: 5px;
+}
+</style>
