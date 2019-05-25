42643b696f11ab9db53bb75e8acdb4078ed698b4
Kaoru <19984244+KunoichiZ@users.noreply.github.com>
2019-05-25 00:18:16 -0500

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,7 @@
 <template>
-  <!-- @todo Add discord link to navbar
-       @body Add Discord invite link to navbar https://discord.gg/EZSxqRr-->
-       
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://www.kunoichiz.me/acnl-modding-info"> Animal Crossing New Leaf Modding Info </a>
+      <a class="navbar-item" href="https://www.acnl-modding.kunoichiz.me"> Animal Crossing New Leaf Modding Info </a>
     </div>
 
     <div id="navbarBasicExample" class="navbar-menu">
@@ -12,6 +9,8 @@
 
       <div class="navbar-end">
         <nuxt-link to="/" class="navbar-item">Home </nuxt-link>
+
+        <nuxt-link to="/installation" class="navbar-item">Plugin Installation </nuxt-link>
 
         <nuxt-link to="/cheatlist" class="navbar-item">Cheat List </nuxt-link>
 
@@ -24,8 +23,6 @@
         <nuxt-link to="/jargon" class="navbar-item">Jargon </nuxt-link>
 
         <nuxt-link to="/faq" class="navbar-item">FAQ </nuxt-link>
-
-        <!-- <a href="https://discord.gg/EZSxqRr"> ACNL Modding Server Invite</a> -->
       </div>
     </div>
   </nav>
@@ -34,5 +31,11 @@
 <style>
 nav {
   background: url('/side_bg.png');
+  border-radius: 15px;
+}
+
+.navbar a:hover {
+  background: url('/side_bg.png');
+  cursor: url('/cursor.cur'), auto;
 }
 </style>
