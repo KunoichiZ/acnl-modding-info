c0490406aa6c7968d5cdde92c22ec367039f6ceb
Kaoru <19984244+KunoichiZ@users.noreply.github.com>
2019-05-27 00:32:50 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
     title: pkg.description,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     script: [
