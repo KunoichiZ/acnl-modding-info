36f203c35a4410e35809840911f8ad55ee9df25c
Kaoru <KunoichiZ@users.noreply.github.com>
2019-06-30 00:55:58 -0500

    Add mobile support and title+description

--- pages/index.vue
+++ pages/index.vue
@@ -19,17 +19,35 @@
       </ul>
     </div>
 
-    <br><br><u>Questions about the site?</u>
+    <br><br><h1 class="is-size-5">Questions about the site?</h1>
     <div id="questions">
       DM <b-tooltip label="AinoMinako#7220" dashed>@KunoichiZ</b-tooltip> on Discord.
     </div>
 
-    <br><br><u>Issues with the site or the content on it?</u>
+    <br><br><h1 class="is-size-5">Issues with the site or the content on it?</h1>
     <div id="issues">
       Open an <a href="https://github.com/KunoichiZ/acnl-modding-info/issues">issue</a> or make a <a href="https://github.com/KunoichiZ/acnl-modding-info/pulls">pull request</a>. When making a pull request, please test all code changes before submitting the pull request. Any changes that fail to build will be automatically rejected until they are fixed. For more information on issues and pull requests, please visit the <a href="https://github.com/KunoichiZ/acnl-modding-info/blob/master/.github/ISSUE_TEMPLATE.md">issue template</a> and the <a href="https://github.com/KunoichiZ/acnl-modding-info/blob/master/.github/PULL_REQUEST_TEMPLATE.md">pull request template</a>.
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Home - ACNL Modding Info'
+    }
+  },
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'description', name: 'description', content: 'Home' }
+      ]
+    }
+  }
+}
+</script>
 
 <style scoped>
 #pages {
@@ -68,4 +86,32 @@
     text-align: left;
 }
 
+@media (max-width: 1300px) {
+  #pages {
+    width: 475px;
+    padding: 10px;
+    border-radius: 25px;
+    border-color: #a1cc00;
+    border-width: 10px;
+    margin: auto;
+  }
+
+  #questions {
+    width: 475px;
+    padding: 10px;
+    border-radius: 25px;
+    border-color: #f58094;
+    border-width: 10px;
+    margin: auto;
+  }
+
+  #issues {
+    width: 475px;
+    padding: 10px;
+    border-radius: 25px;
+    border-color: #41aed7;
+    border-width: 10px;
+    margin: auto;
+  }
+}
 </style>
