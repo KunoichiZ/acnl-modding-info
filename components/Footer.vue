137c50fbf3bf4e6647e79f683ee44d91e6a68fd9
Kaoru <KunoichiZ@users.noreply.github.com>
2019-06-30 18:35:16 -0500

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div class="content has-text-centered">
       <p>Website © 2019 KunoichiZ</p>
-      <p>Website generated with &#x2764; by <a href="https://nuxtjs.org/">Nuxt.js</a></p>
+      <p>Website generated with &#x2764; by <a href="https://nuxtjs.org/">Nuxt.js</a> and <a href="https://bulma.io/">Bulma</a></p>
     </div>
   </footer>
 </template>
