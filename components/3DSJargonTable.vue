c06ff81c2adebbd83a126734b0a80baf412fc064
Kaoru <KunoichiZ@users.noreply.github.com>
2019-06-30 23:37:08 -0500

    Create 3DSJargonTable.vue

---
+++ components/3DSJargonTable.vue
@@ -0,0 +1,101 @@
+<template>
+    <table id="3dsjargon">
+        <tr>
+            <th>Term</th>
+            <th>Description</th> 
+        </tr>
+        <tr>
+            <td>amiibo</td>
+            <td>Series of figures by Nintendo that contain an NFC chip used to communicate with 3DS and WiiU consoles. On the new Nintendo 3DS, the Amiibo can be connected by placing the figure on the bottom screen. For the old 3DS, a separate adapter needs to be bought, as it does not have NFC capabilities.</td> 
+        </tr>
+        <tr>
+            <td>Boot9Strap (B9S)</td>
+            <td>A modified firmware that makes use of sighax and a vulnerability in the console's NDMA engine to gain code execution on the ARM9 BootROM (Boot9) and load a payload ("boot.firm") from the SD card. It uses a modified version the ARM9 payload loader of Luma3DS. The software can also dump the console-specific OTP and BootROMs.</td> 
+        </tr>
+        <tr>
+            <td>Cartridge (cart)</td>
+            <td>A plastic casing containing variable content, for example, a PCB containing a game held on ROM including a backup EEPROM/battery to hold the game save. Here, this term is commonly used to describe a 3DS game card or any other device used to slot into the expansion slots of a handheld, even flash kit adapters.</td>
+        <tr>
+            <td>CFW</td>
+            <td>Short for Custom FirmWare. While this term can be debated until the cows come home, in the 3DS scene, it typically means when you're running NATIVE_FIRM with signatures disabled.</td> 
+        </tr>
+        <tr>
+            <td>CIA</td>
+            <td>This stands for CTR Importable Archive. It's like a WAD file from the Wii days; it's just a container that contains game files or system firmware updates. Installing these will either install a game (to the SD card) or parts of a system firmware update to your 3DS' NAND (if on emuNAND, then it'll obviously install to the SD card's emuNAND).</td>
+        </tr>
+        <tr>
+            <td>eShop</td>
+            <td>An online store for the Nintendo 3DS and Wii U where digital games, demos, and other things can be downloaded.</td>
+        </tr>
+        <tr>
+            <td>Firmware</td>
+            <td>The code that a device runs from. A device which allows for firmware upgrades means that a new version of the device doesn't need to be bought to get new features.</td>
+        </tr>
+        <tr>
+            <td>Luma3DS</td>
+            <td>A popular Custom Firmware (CFW) for the Nintendo 3DS that sports a variety of features, such as payload chaining and on-the-fly game patching.</td>
+        </tr>
+        <tr>
+            <td>Micro SD</td>
+            <td>A smaller version of the SD card. About half the size of a mini SD and about a quarter of the size of a normal SD card. Can hold just as much data as a normal SD card.</td>
+        </tr>
+        <tr>
+            <td>N3DS/New3DS</td>
+            <td>"New" 3DS. As in, the model that Nintendo released in late 2014 with Super Stable 3D, the C-stick, and ZL/ZR buttons, and an 804Mhz CPU speed. The New 2DS is the same, except with no 3D features.</td>
+        </tr>
+        <tr>
+            <td>Nintendo Network</td>
+            <td>A network of servers by Nintendo used for online play.</td>
+        </tr>
+        <tr>
+            <td>Nintendo Network ID (NNID)</td>
+            <td>An unique ID used by people to identify on the Nintendo Network. Intended to replace the older friend codes system. Can only be used on 1 Nintendo 3DS console at a time.</td>
+        </tr>
+        <tr>
+            <td>O3DS/Old3DS</td>
+            <td>"Old" 3DS. Any 3DS model that isn't a "New" 3DS and runs at only 268MHz. This terminology includes the original 2DS, usually.</td>
+        </tr>
+        <tr>
+            <td>OFW</td>
+            <td>Official/original firmware. The unmodified firmware installed on your console, without any hacks applied.</td>
+        </tr>
+        <tr>
+            <td>SD Card</td>
+            <td>Acronym for Secure Digital cards. It is a flash memory card format used in the 3DS and several other portable devices.</td>
+        </tr>
+        <tr>
+            <td>SDHC</td>
+            <td>SD High Capacity. Refers to SD cards that are larger than 2GB, up to 32GB.</td>
+        </tr>
+        <tr>
+            <td>SDXC</td>
+            <td>SD eXtended Capacity. Refers to SD cards that are larger than 32GB, up to 2TB. The 3DS supports SDXC cards up to 128GB if formatted as FAT32.</td>
+        </tr>
+        <tr>
+            <td>sighax</td>
+            <td>An exploit, using a flaw in the RSA signature checks of the ARM9 BootROM (Boot9) to load unofficial firmware images.</td>
+        </tr>
+        <tr>
+            <td>System Menu</td>
+            <td>The menu used on the 3DS.</td>
+        </tr>
+        <tr>
+            <td>Title</td>
+            <td>A title is an application for the 3DS, which has a unique identifier. Examples are 3DS games and system applications such as Mii plaza.</td>
+        </tr>
+        <tr>
+            <td>Wi-Fi</td>
+            <td>Short for "Wireless fidelity". Wi-Fi is a type of wireless LAN. This makes it possible for a Wi-Fi enabled device, such as the Nintendo 3DS, to access an internet connection shared via a wireless LAN without any additional wires or connections.</td>
+        </tr>
+    </table>
+</template>
+
+<style scoped>
+table, td, th {
+    border: 1px solid black;
+}
+
+th, td {
+    padding: 15px;
+}
+</style>
