e03cd3eacdf9d9aecb1075adc4f981e6eff1b94a
Kaoru <KunoichiZ@users.noreply.github.com>
2019-07-01 00:08:44 -0500

    Add jargon to Jargon page

--- pages/Jargon.vue
+++ pages/Jargon.vue
@@ -1,17 +1,36 @@
 <template>
   <section class="container has-text-centered">
-    <div>Jargon</div>
-  
-     // TODO: Add 3DS and ACNL Jargon
+    <br><h1 class="title is-size-3">Jargon</h1>
+    <p>This is a table of terminology aka jargon related to both the 3DS and ACNL. Some of the terms aren't mentioned in the website itself, but are good to know regardless. Terms have been extracted from two GBAtemp links, the <a href="https://gbatemp.net/threads/glossary-of-common-3ds-hacking-terms.414430/">Glossary of Common 3DS Hacking Terms</a> and the <a href="https://wiki.gbatemp.net/wiki/3DS_Glossary">3DS Glossary</a> on the GBAtemp wiki.</p>
+    <br>
+    <b-tabs type="is-toggle-rounded" size="is-medium" position="is-centered" v-model="activeTab">
+        <b-tab-item icon="gamepad" label="3DS Jargon">
+            <jargon3DS></jargon3DS>
+        </b-tab-item>
+
+        <b-tab-item icon="leaf" label="ACNL Jargon">
+            <jargonACNL></jargonACNL>
+        </b-tab-item>
+    </b-tabs>
+     // TODO: Add ACNL Jargon
+     // TODO: Add CSS to Jargon tables
 
   </section>
 </template>
 
 <script>
+import jargon3DS from "~/components/3DSJargonTable.vue";
+import jargonACNL from "~/components/ACNLJargonTable.vue";
+
 export default {
+  components: {
+    jargon3DS,
+    jargonACNL
+  }, 
   data () {
     return {
-      title: '3DS and ACNL Jargon'
+      title: '3DS and ACNL Jargon',
+      activeTab: 0
     }
   },
   head () {
