50f4b63c68cb8e588704ed4e6d3785e6d4dfe577
Kaoru <KunoichiZ@users.noreply.github.com>
2019-07-01 00:07:47 -0500

    Add starting ACNL jargon

---
+++ components/ACNLJargonTable.vue
@@ -0,0 +1,46 @@
+<template>
+    <table id="acnljargon">
+        <tr>
+            <th>Term</th>
+            <th>Description</th> 
+        </tr>
+        <tr>
+            <td>ACNL</td>
+            <td>Stands for Animal Crossing New Leaf.</td> 
+        </tr>
+        <tr>
+            <td>NLSE</td>
+            <td>NLSE is an offline ACNL save editor which functions the same as the online editor and more.</td>
+        </tr>
+        <tr>
+            <td>NLTK</td>
+            <td>New Leaf Toolkit. It's a WIP ACNL Toolkit/Save Editor without the hassle of taking out your SD card. Only CFW is needed to use it.</td>
+        </tr>
+        <tr>
+            <td>QM</td>
+            <td>Quick Menu. Quick Menu gives quicker access to some cheats, such as watering flowers, setting the time, modifying your appearance, give yourself a furniture set, and more.</td>
+        </tr>
+        <tr>
+            <td>RTBM</td>
+            <td>Real Time Building Modifier. Allows you to place, remove, or move a building.</td> 
+        </tr>
+        <tr>
+            <td>RTWE</td>
+            <td>Real Time World Editor. Allows you to edit your ACNL town in real time.</td>
+        </tr>
+        <tr>
+            <td>Seeding</td>
+            <td>Seeding is a term used for the general act of being able to place buildings and items in online play. This includes items such as trees, rocks, buildings (such as Town Hall and houses), so it can be very detrimental for a player that can't do anything about it.</td>
+        </tr>
+    </table>
+</template>
+
+<style scoped>
+table, td, th {
+    border: 1px solid black;
+}
+
+th, td {
+    padding: 15px;
+}
+</style>
