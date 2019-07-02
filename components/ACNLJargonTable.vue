0df6c08067d6abe0798db0043d560078be500654
Kaoru <KunoichiZ@users.noreply.github.com>
2019-07-02 15:39:25 -0500

    Add Orig and WA terms to ACNL jargon

--- components/ACNLJargonTable.vue
+++ components/ACNLJargonTable.vue
@@ -17,6 +17,10 @@
             <td>New Leaf Toolkit. It's a WIP ACNL Toolkit/Save Editor without the hassle of taking out your SD card. Only CFW is needed to use it.</td>
         </tr>
         <tr>
+            <td>Orig</td>
+            <td>Original means the version of Animal Crossing New Leaf that has Welcome Amiibo installed from an update. Its Home Menu icon looks like this: <br><img src="https://cdn.discordapp.com/attachments/275462725688229888/498974563367911445/acnl.png"></td>
+        </tr>
+        <tr>
             <td>QM</td>
             <td>Quick Menu. Quick Menu gives quicker access to some cheats, such as watering flowers, setting the time, modifying your appearance, give yourself a furniture set, and more. You hold R + X for a small bit when you're not in any other menu to open the Quick Menu.</td>
         </tr>
@@ -31,6 +35,10 @@
         <tr>
             <td>Seeding</td>
             <td>Seeding is a term used for the general act of being able to place buildings and items in online play. This includes items such as trees, rocks, buildings (such as Town Hall and houses), so it can be very detrimental for a player that can't do anything about it.</td>
+        </tr>
+        <tr>
+            <td>WA</td>
+            <td>WA means Welcome Amiibo. This version has Welcome Amiibo built into the game. Its Home Menu icon looks like this: <br><img src="https://cdn.discordapp.com/attachments/275462725688229888/497798468379607041/acwa.png"></td>
         </tr>
     </table>
 </template>
