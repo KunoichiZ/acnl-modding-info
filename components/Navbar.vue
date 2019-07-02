cb67ab109149d982305faddc4d21ba96c417f56b
Kaoru <KunoichiZ@users.noreply.github.com>
2019-07-02 18:44:38 -0500

    Remove spaces in Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -14,21 +14,21 @@
       <div class="navbar-start"></div>
 
       <div class="navbar-end">
-        <nuxt-link to="/" class="navbar-item">Home </nuxt-link>
+        <nuxt-link to="/" class="navbar-item">Home</nuxt-link>
 
-        <nuxt-link to="/installation" class="navbar-item">Plugin Installation </nuxt-link>
+        <nuxt-link to="/installation" class="navbar-item">Plugin Installation</nuxt-link>
 
-        <nuxt-link to="/cheatlist" class="navbar-item">Cheat List </nuxt-link>
+        <nuxt-link to="/cheatlist" class="navbar-item">Cheat List</nuxt-link>
 
-        <nuxt-link to="/releases" class="navbar-item">Releases </nuxt-link>
+        <nuxt-link to="/releases" class="navbar-item">Releases</nuxt-link>
 
-        <nuxt-link to="/itemids" class="navbar-item">Item IDs </nuxt-link>
+        <nuxt-link to="/itemids" class="navbar-item">Item IDs</nuxt-link>
 
-        <nuxt-link to="/guides" class="navbar-item">Guides </nuxt-link>
+        <nuxt-link to="/guides" class="navbar-item">Guides</nuxt-link>
 
-        <nuxt-link to="/jargon" class="navbar-item">Jargon </nuxt-link>
+        <nuxt-link to="/jargon" class="navbar-item">Jargon</nuxt-link>
 
-        <nuxt-link to="/faq" class="navbar-item">FAQ </nuxt-link>
+        <nuxt-link to="/faq" class="navbar-item">FAQ</nuxt-link>
       </div>
     </div>
   </nav>
