4f71c7e9d3b04cd4e58641e4ead3db4bc519b476
Kaoru <KunoichiZ@users.noreply.github.com>
2019-07-02 20:09:15 -0500

    Update Guides.vue

--- pages/Guides.vue
+++ pages/Guides.vue
@@ -5,26 +5,20 @@
     These are guides to help people either obtain CFW or find out how to obtain the plugin.
     <br><br>
     <h1 class="title is-size-4">CFW Guide</h1>
-    <div id="cfw">A complete guide on obtaining CFW can be found at <a href="https://3ds.hacks.guide/">https://3ds.hacks.guide/</a>.</div>
+    <div id="cfw">A complete guide on obtaining CFW can be found at <a href="https://3ds.hacks.guide/">https://3ds.hacks.guide/</a>. If you need help with obtaining CFW, please join the Please join the <a href="https://discord.gg/C29hYvh">Nintendo Homebrew Discord Server.</a></div>
     <br>
     <h1 class="title is-size-4">How to Obtain the Plugin</h1>
     There are a couple of ways to find the latest version of the plugin. 
     <div id="obtain">
       <ol>
         <li><nuxt-link to="/releases">Releases</nuxt-link>: This page will contain the latest version of the plugin.</li>
-        <li>If you are on the <a href="#acnlserver">ACNL Modding Discord Server</a>, go to the #ntr-plugin-info channel and you should see the latest version of the plugin as the most recent post. Also, if you are in the server, you can use the bot command !plgupdates in the #bot-commands channel to gain access to the PlgUpdates role, which the mods will ping when there is a new update to the plugin.</li>
+        <li>If you are on the ACNL Modding Discord Server, go to the #ntr-plugin-info channel and you should see the latest version of the plugin as the most recent post. Also, if you are in the server, you can use the bot command !plgupdates in the #bot-commands channel to gain access to the PlgUpdates role, which the mods will ping when there is a new update to the plugin.</li>
         <li>You can also go to the releases tab on the <a href="https://github.com/rydoginator/ACNL-NTR-Cheats/releases/latest">GitHub page for the ACNL Plugin</a> and find the latest release.</li>
       </ol>
+      <br>
+      Need more help on the plugin or with ACNL modding? Please join the ACNL Modding Discord Server.
+      <br><a href="https://discord.gg/EZSxqRr"><img src="https://discordapp.com/api/guilds/275461571231219712/widget.png?style=banner3" alt="Join ACNL Modding Discord Server"/></a>
     </div>
-    <br>
-    <h1 class="title is-size-4">Nintendo Homebrew Discord Server</h1>
-    <div id="nserver">Need more help on getting CFW? Please join the <a href="https://discord.gg/C29hYvh">Nintendo Homebrew Discord Server.</a></div>
-    <!-- <a href="https://discord.gg/C29hYvh"><img src="https://discordapp.com/api/guilds/196618637950451712/widget.png?style=banner3" alt="Join Nintendo Homebrew Discord Server"/></a></div> -->
-    
-    <br><br>
-    <h1 class="title is-size-4" id="acnlserver">Animal Crossing New Leaf Modding Discord Server</h1>
-    <div id="aserver">Need more help on the plugin or with ACNL modding? Please join the ACNL Modding Discord Server.
-    <br><a href="https://discord.gg/EZSxqRr"><img src="https://discordapp.com/api/guilds/275461571231219712/widget.png?style=banner3" alt="Join ACNL Modding Discord Server"/></a></div>
      
     <!-- <br>
     <br>
