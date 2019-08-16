478ef6358692adb0dd3db036f16aaa94b30f57d9
Kaoru <KunoichiZ@users.noreply.github.com>
2019-08-15 21:14:55 -0500

    Re-add Garden table

--- components/ItemIDsMenu.vue
+++ components/ItemIDsMenu.vue
@@ -8,23 +8,24 @@
                 <li><a href="#art" id="link-art" @click="selected = 1" :class="{highlight:selected == 1}">Art</a></li>
                 <li><a href="#catchables" id="link-catchables" @click="selected = 2" :class="{highlight:selected == 2}">Catchables</a></li>
                 <li><a href="#clothes" id="link-clothes" @click="selected = 3" :class="{highlight:selected == 3}">Clothes</a></li>
-                <li><a href="#patterns" id="link-patterns" @click="selected = 4" :class="{highlight:selected == 4}">Custom Patterns</a></li>
-                <li><a href="#environment" id="link-environment" @click="selected = 5" :class="{highlight:selected == 5}">Environmentals</a></li>
+                <li><a href="#patterns" id="link-patterns" @click="selected = 4" :class="{highlight:selected == 4}">Custom Patterns <i class="fab fa-get-pocket green"></i></a></li>
+                <li><a href="#environment" id="link-environment" @click="selected = 5" :class="{highlight:selected == 5}">Environmentals <i class="fab fa-get-pocket red"></i></a></li>
                 <li><a href="#fortunes" id="link-fortunes" @click="selected = 6" :class="{highlight:selected == 6}">Fortune Cookies</a></li>
                 <li><a href="#fossils" id="link-fossils" @click="selected = 7" :class="{highlight:selected == 7}">Fossils</a></li>
                 <li><a href="#fruit" id="link-fruit" @click="selected = 8" :class="{highlight:selected == 8}">Fruits</a></li>
                 <li><a href="#furniture" id="link-furniture" @click="selected = 9" :class="{highlight:selected == 9}">Furniture</a></li>
-                <li><a href="#gyroids" id="link-gyroids" @click="selected = 10" :class="{highlight:selected == 10}">Gyroids</a></li>
-                <li><a href="#hra" id="link-hra" @click="selected = 11" :class="{highlight:selected == 11}">HRA Exhibition (Inside Only)</a></li>
-                <li><a href="#events" id="link-events" @click="selected = 12" :class="{highlight:selected == 12}">In-Game Events</a></li>
-                <li><a href="#sprites" id="link-sprites" @click="selected = 13" :class="{highlight:selected == 13}">Item Sprites</a></li>
-                <li><a href="#misc" id="link-misc" @click="selected = 14" :class="{highlight:selected == 14}">Misc</a></li>
-                <li><a href="#npc" id="link-npc" @click="selected = 15" :class="{highlight:selected == 15}">NPC Items</a></li>
-                <li><a href="#paper" id="link-paper" @click="selected = 16" :class="{highlight:selected == 16}">Paper</a></li>
-                <li><a href="#pictures" id="link-pictures" @click="selected = 17" :class="{highlight:selected == 17}">Pictures</a></li>
-                <li><a href="#songs" id="link-songs" @click="selected = 18" :class="{highlight:selected == 18}">Songs</a></li>
-                <li><a href="#tools" id="link-tools" @click="selected = 19" :class="{highlight:selected == 19}">Tools & Wearables</a></li>
-                <li><a href="#wallsCarpets" id="link-wallsCarpets" @click="selected = 20" :class="{highlight:selected == 20}">Wallpapers & Carpets</a></li>
+                <li><a href="#garden" id="link-furniture" @click="selected = 10" :class="{highlight:selected == 10}">Garden</a></li>
+                <li><a href="#gyroids" id="link-gyroids" @click="selected = 11" :class="{highlight:selected == 11}">Gyroids</a></li>
+                <li><a href="#hra" id="link-hra" @click="selected = 12" :class="{highlight:selected == 12}">HRA Exhibition (Inside Only)</a></li>
+                <li><a href="#events" id="link-events" @click="selected = 13" :class="{highlight:selected == 13}">In-Game Events</a></li>
+                <li><a href="#sprites" id="link-sprites" @click="selected = 14" :class="{highlight:selected == 14}">Item Sprites</a></li>
+                <li><a href="#misc" id="link-misc" @click="selected = 15" :class="{highlight:selected == 15}">Misc</a></li>
+                <li><a href="#npc" id="link-npc" @click="selected = 16" :class="{highlight:selected == 16}">NPC Items</a></li>
+                <li><a href="#paper" id="link-paper" @click="selected = 17" :class="{highlight:selected == 17}">Paper</a></li>
+                <li><a href="#pictures" id="link-pictures" @click="selected = 18" :class="{highlight:selected == 18}">Pictures</a></li>
+                <li><a href="#songs" id="link-songs" @click="selected = 19" :class="{highlight:selected == 19}">Songs</a></li>
+                <li><a href="#tools" id="link-tools" @click="selected = 20" :class="{highlight:selected == 20}">Tools & Wearables</a></li>
+                <li><a href="#wallsCarpets" id="link-wallsCarpets" @click="selected = 21" :class="{highlight:selected == 21}">Wallpapers & Carpets</a></li>
             </ul>
         </ul>
     </aside>
@@ -53,7 +54,7 @@ export default {
     padding: 10px;
     background: url('/yellow_stripe_bg.png');
     /* width: 500px; */
-    height: 790px;
+    height: 810px;
     overflow-y:hidden;
 }
 
@@ -70,5 +71,13 @@ export default {
 a:hover {
     border-radius: 25px;
     background: url('/bg_inner.png');
+}
+
+.red {
+    color: red;
+}
+
+.green {
+    color: green;
 }
 </style>
