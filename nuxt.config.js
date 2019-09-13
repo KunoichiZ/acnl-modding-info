4ebcbc2ab212dab96540a898931f973c6b9b8b88
Kaoru <KunoichiZ@users.noreply.github.com>
2019-09-13 16:39:04 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+      { hid: 'title', property: 'title', content: 'ACNL Modding Info'},
       { hid: 'og-description', name: 'og:description', content: pkg.description },
       { hid: 'og-title', property: 'og:title', content: 'ACNL Modding Info' },
       { hid: 'og-site_name', property: 'og:site_name', content: 'ACNL Modding Info'},
