15b6df038146868568e067a6c1a67304a2e26a70
Kaoru <KunoichiZ@users.noreply.github.com>
2020-01-15 13:57:21 -0600

    Update Releases.vue

--- pages/Releases.vue
+++ pages/Releases.vue
@@ -38,7 +38,9 @@
         <br>
         <div id="fourpoint2b2">
             <h6 class="title is-6"><a href="https://cdn.discordapp.com/attachments/360861723361542144/663907066779271212/ACNL-NTR-Cheats.zip">Version 4.2 Beta 1</a> | <a href="#toc">(back to Table of Contents)</a></h6>
-            
+            <div class="box">
+                <i class="fas fa-exclamation-triangle"></i> WARNING: This update does not work with BootNTR! <i class="fas fa-exclamation-triangle"></i>
+            </div>
             New Features
             <ul>
                 <li>Fill museum thanks @rydoginator</li>
@@ -60,17 +62,16 @@
 
             <br>So: 
             <ul>
-                <li>Download Luma attached <a href="https://cdn.discordapp.com/attachments/360861723361542144/659269638936461315/boot.firm">here</a></li>
+                <li>Download Luma from the GitHub page here: <a href="https://github.com/mariohackandglitch/Luma3DS/releases">https://github.com/mariohackandglitch/Luma3DS/releases</a></li>
                 <li>Extract this zip to the root of your SD card</li>
                 <li>Boot your 3DS</li>
                 <li>Press L + Select + D Pad Down to open Roselina Settings, then choose plugin loading</li>
                 <li>Launch Animal Crossing New Leaf and once you select your player, choose any cheats that you want!</li>
             </ul>
             <br>
-            <p>I forgot to downgrade the lib of the plugin for the release, so if you'd like to have ctrpf 0.5.0, try <a href="https://cdn.discordapp.com/attachments/360861723361542144/663962571568709643/ACNL-NTR-Cheats.zip">this version</a> of the plugin.
-            <br>This one is a loooot more stable than the last one, the previous lib was experimental, as we expected @Nanquitas to work on it more, but he got busy. 
-            <br>You can use this with the plugin loader found <a href="https://github.com/mariohackandglitch/Luma3DS/releases">here</a>.</p>
             <b>Download links:</b> <a href="https://cdn.discordapp.com/attachments/360861723361542144/663907066779271212/ACNL-NTR-Cheats.zip">4.2 Beta 1</a>
+            <br><br><p>I forgot to downgrade the lib of the plugin for the release, so if you'd like to have ctrpf 0.5.0, try <a href="https://cdn.discordapp.com/attachments/360861723361542144/663962571568709643/ACNL-NTR-Cheats.zip">this version</a> of the plugin.
+            <br>This one is a loooot more stable than the last one, the previous lib was experimental, as we expected @Nanquitas to work on it more, but he got busy. </p>
         </div>
         <br>
         <div id="fourpoint2b1">
@@ -686,6 +687,10 @@ export default {
 
 #threepoint1point2 ol {
   list-style-position: inside;
+}
+
+.box {
+  background-color: yellow;
 }
 
 @media (max-width: 1300px) {
